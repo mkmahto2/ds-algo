405b6b77675856799a171c4bbe5cdc294cd9b928
pr2tik1 <pr2tik1@gmail.com>
2020-06-27 23:14:15 +0530

    last digit of fibonacci

--- algo-toolbox/assignments/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
+++ algo-toolbox/assignments/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+using std::vector;
 
 int get_fibonacci_last_digit_naive(int n) {
     if (n <= 1)
@@ -16,9 +18,21 @@ int get_fibonacci_last_digit_naive(int n) {
     return current % 10;
 }
 
+int get_fibonacci_last_digit(int n) {
+  vector<int> result(n+1);
+  result[0] = 0;
+  result[1] = 1;
+  for(int i=2; i< result.size();i++) {
+	  result[i] = (result[i - 1] + result[i - 2]) % 10;
+  }
+  return result[n];
+}
+
 int main() {
     int n;
     std::cin >> n;
-    int c = get_fibonacci_last_digit_naive(n);
+    //int c = get_fibonacci_last_digit_naive(n);
+    int c = get_fibonacci_last_digit(n);
+    //std::cout << "Naive/Slower Algot output: "<< c << '\n';
     std::cout << c << '\n';
     }
--- algo-toolbox/assignments/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
+++ algo-toolbox/assignments/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
@@ -13,7 +13,15 @@ def get_fibonacci_last_digit_naive(n):
 
     return current % 10
 
+def get_fibonacci_last_digit(n):
+    result = [0]*(n+1)
+    result[0] = 0
+    result[1] = 1
+    for i in range(2,n+1):
+        result[i] = (result[i-1] + result[i-2]) % 10
+    return result[n]
+
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n = int(input)
-    print(get_fibonacci_last_digit_naive(n))
+    #input = sys.stdin.read()
+    n = int(input())
+    print(get_fibonacci_last_digit(n))
