8f4709b686e8f10c99f59e8c4101730acef4c2d6
pr2tik1 <pr2tik1@gmail.com>
2020-06-27 23:59:32 +0530

    lcm solution

--- algo-toolbox/assignments/week2_algorithmic_warmup/4_least_common_multiple/lcm.cpp
+++ algo-toolbox/assignments/week2_algorithmic_warmup/4_least_common_multiple/lcm.cpp
@@ -8,9 +8,24 @@ long long lcm_naive(int a, int b) {
   return (long long) a * b;
 }
 
+
+long long gcd(long long a, long long b) {
+  //write your code here
+  	if (b == 0)
+		return a;
+	return gcd(b, a%b);
+}
+
+long long lcm(long long a, long long b) {
+  //write your code here
+  return a*b / gcd(a,b);
+}
+
+
 int main() {
   int a, b;
   std::cin >> a >> b;
-  std::cout << lcm_naive(a, b) << std::endl;
+  //std::cout << lcm_naive(a, b) << std::endl;
+  std::cout << lcm(a, b) << std::endl;
   return 0;
 }
--- algo-toolbox/assignments/week2_algorithmic_warmup/4_least_common_multiple/lcm.py
+++ algo-toolbox/assignments/week2_algorithmic_warmup/4_least_common_multiple/lcm.py
@@ -8,8 +8,17 @@ def lcm_naive(a, b):
 
     return a*b
 
+def gcd(a,b):
+    if a ==0 :
+        return b
+    return gcd(b%a, a)
+
+def lcm(a,b):
+    result = int(a*b/gcd(a,b))
+    return result
+
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    a, b = map(int, input.split())
-    print(lcm_naive(a, b))
+    #input = sys.stdin.read()
+    a, b = map(int, input().split())
+    print(lcm(a, b))
 
