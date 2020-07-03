a025dcb18ded96ff1d5a2bf99ce7c86e6b510e16
pr2tik1 <pr2tik1@gmail.com>
2020-07-03 22:43:16 +0530

    knapsnack solution

--- algo-toolbox/assignments/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ algo-toolbox/assignments/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -1,17 +1,43 @@
 # Uses python3
 import sys
 
+def get_best_idx(ratios):
+    best = 0.0
+    best_idx = 0
+    for i in range(len(ratios)):
+        if ratios[i] > best:
+            best = ratios[i]
+            best_idx = i
+    return best_idx
+
+
 def get_optimal_value(capacity, weights, values):
     value = 0.
     # write your code here
+    ratios = [0.]*len(weights)
+    for i in range(len(weights)):
+        ratios[i] = float(values[i]/weights[i])
+    
+    while (capacity > 0):
+        best_idx = get_best_idx(ratios)
+        if (weights[best_idx] > 0):
+            a = min(float(capacity), float(weights[best_idx]))
+            ratio = float(values[best_idx])/ weights[best_idx]
+            value += a * ratio
+            weights[best_idx] -= a
+            capacity -= a
 
     return value
 
 
 if __name__ == "__main__":
-    data = list(map(int, sys.stdin.read().split()))
+    print("Enter the divisions and limit")
+    data = list(map(int, input().split()))
     n, capacity = data[0:2]
-    values = data[2:(2 * n + 2):2]
-    weights = data[3:(2 * n + 2):2]
+    values = [0]*n
+    weights = [0]*n
+    for i in range(n):
+        print("Enter the values and weights:")
+        values[i], weights[i] = map(int, input().split())
     opt_value = get_optimal_value(capacity, weights, values)
-    print("{:.10f}".format(opt_value))
+    print(int(opt_value))
