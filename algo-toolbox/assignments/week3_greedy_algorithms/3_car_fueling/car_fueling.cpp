699de19ca05157ca4b0a55bedc6d195f6b478c1e
pr2tik1 <pr2tik1@gmail.com>
2020-07-09 14:28:58 +0530

    week3 solutions

--- algo-toolbox/assignments/week3_greedy_algorithms/3_car_fueling/car_fueling.cpp
+++ algo-toolbox/assignments/week3_greedy_algorithms/3_car_fueling/car_fueling.cpp
@@ -3,16 +3,42 @@
 
 using std::cin;
 using std::cout;
-using std::vector;
 using std::max;
+using std::vector;
 
-int compute_min_refills(int dist, int tank, vector<int> & stops) {
+int compute_min_refills(int dist, int tank, vector<int> &stops, int count = 0)
+{
     // write your code here
+    int rCount = 0;
+    int refillPos = -2;
+    int i = 0;
+    int currLocation = 0;
+    stops.push_back(dist);
+    while (true)
+    {
+        if (tank + currLocation >= dist)
+        {
+            return rCount;
+        }
+        if (tank < stops[i] - currLocation)
+        {
+            refillPos = i - 1;
+            if (currLocation == stops[refillPos])
+            {
+                return -1;
+            }
+            rCount++;
+            currLocation = stops[i - 1];
+            i -= 1;
+            continue;
+        }
+        i++;
+    }
     return -1;
 }
 
-
-int main() {
+int main()
+{
     int d = 0;
     cin >> d;
     int m = 0;
--- algo-toolbox/assignments/week3_greedy_algorithms/3_car_fueling/car_fueling.py
+++ algo-toolbox/assignments/week3_greedy_algorithms/3_car_fueling/car_fueling.py
@@ -4,6 +4,10 @@ import sys
 
 def compute_min_refills(distance, tank, stops):
     # write your code here
+    num_refill = 0
+    current_refill = 0
+    while current_refill <= :
+        return 
     return -1
 
 if __name__ == '__main__':
